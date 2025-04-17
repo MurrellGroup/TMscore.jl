7ad689a7df67435eb65fac11ac68f58e30d0d326
Anton Oresten <antonoresten@gmail.com>
2025-04-17 23:44:57 +0200

    Update runtests.jl

--- test/runtests.jl
+++ test/runtests.jl
@@ -10,14 +10,11 @@ using BioStructures
         downloadpdb(["1CRN", "1EJG"]; dir, format=MMCIFFormat)
         struc1 = read("$dir/1CRN.pdb", PDBFormat)
         struc2 = read("$dir/1EJG.pdb", PDBFormat)
-        @test allequal(tms -> tms.tmscore,
-            [
-                tmscore("$dir/1CRN.pdb", "$dir/1EJG.pdb"),
-                tmscore("$dir/1CRN.cif", "$dir/1EJG.cif"),
-                tmscore(struc1, struc2),
-                tmscore(struc1, "$dir/1EJG.cif")
-            ]
-        )
+        @test allequal([
+            tmscore("$dir/1CRN.pdb", "$dir/1EJG.pdb").tmscore,
+            tmscore("$dir/1CRN.cif", "$dir/1EJG.cif").tmscore,
+            tmscore(struc1, struc2).tmscore,
+            tmscore(struc1, "$dir/1EJG.cif").tmscore])
     end
 
 end
